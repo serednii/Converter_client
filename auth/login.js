89c8423d9d8f938fd3e5f72c4636e3b32373d972
Serednii Mykola <seredniimykola@gmail.com>
2024-12-13 09:12:16 +0100

    Updates

--- auth/login.js
+++ auth/login.js
@@ -26,7 +26,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
         if (response.ok) {
             console.log(user)
             sessionStorage.setItem("user", JSON.stringify({ ...user, email }))
-            window.location.href = '/';
+            window.location.href = './';
         } else {
             alert(`Помилка: ${data.message}`);
         }
