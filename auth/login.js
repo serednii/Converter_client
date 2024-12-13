8af3bea7c770cba7477dacc664453ee11284c955
Serednii Mykola <seredniimykola@gmail.com>
2024-12-13 09:38:33 +0100

    Updates

--- auth/login.js
+++ auth/login.js
@@ -11,28 +11,28 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     const email = document.getElementById('loginEmail').value;
     const password = document.getElementById('loginPassword').value;
     console.log(email, password)
-    try {
+    // try {
 
-        const response = await fetch(`${urlMainServer}/user/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email, password })
-        });
+    //     const response = await fetch(`${urlMainServer}/user/login`, {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({ email, password })
+    //     });
 
-        const user = await response.json();
+    //     const user = await response.json();
 
-        if (response.ok) {
-            console.log(user);
-            sessionStorage.setItem("user", JSON.stringify({ ...user, email }));
-            // window.location.href = 'https://serednii.github.io/Converter_client';
-            console.log(window.location.href)
-        } else {
-            alert(`Помилка: ${data.message}`);
-        }
+    //     if (response.ok) {
+    //         console.log(user);
+    //         sessionStorage.setItem("user", JSON.stringify({ ...user, email }));
+    //         // window.location.href = 'https://serednii.github.io/Converter_client';
+    //         console.log(window.location.href)
+    //     } else {
+    //         alert(`Помилка: ${data.message}`);
+    //     }
 
-    } catch (error) {
-        alert('Не вдалося підключитися до сервера');
-    }
+    // } catch (error) {
+    //     alert('Не вдалося підключитися до сервера');
+    // }
 });
